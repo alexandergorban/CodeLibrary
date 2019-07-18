80f41ae026f0de20fa6df694e62f81c238b47132
Alexander Gorban <gorban.alexander.y@gmail.com>
2019-07-18 15:22:05 +0300

    P1 Two Sum

---
+++ LeetCode/Solution/P1_TwoSum/SolutionTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace LeetCode.Solution.P1_TwoSum
+{
+    public class SolutionTest
+    {
+        [Fact]
+        public void TwoSum()
+        {
+            int[] nums = new int[] { 2, 7, 11, 15 };
+            int target = 9;
+
+            Assert.Equal(new int[] { 0, 1 }, new Solution().TwoSum(nums, target));
+        }
+    }
+}
