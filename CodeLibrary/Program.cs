4f90d9540938d637275a61ace780ddf5012bd319
Alexander Gorban <gorban.alexander.y@gmail.com>
2019-07-18 03:31:50 +0300

    Initial commit

---
+++ CodeLibrary/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CodeLibrary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
