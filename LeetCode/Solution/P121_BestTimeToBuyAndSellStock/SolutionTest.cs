ea33dc622ddd3179b3b43c58a196b6e0f7a998dd
Alexander Gorban <gorban.alexander.y@gmail.com>
2019-07-19 02:21:57 +0300

    P121 Best Time to Buy and Sell Stock

---
+++ LeetCode/Solution/P121_BestTimeToBuyAndSellStock/SolutionTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace LeetCode.Solution.P121_BestTimeToBuyAndSellStock
+{
+    public class SolutionTest
+    {
+        [Fact]
+        public void MaxProfit()
+        {
+            int[] arrInts1 = new int[] { 7, 1, 5, 3, 6, 4 };
+            int[] arrInts2 = new int[] { 7, 6, 4, 3, 1 };
+
+            Assert.Equal(5, new Solution().MaxProfit(arrInts1));
+            Assert.Equal(0, new Solution().MaxProfit(arrInts2));
+        }
+    }
+}
