7bf48200a069eb3573717e7df28866aa843a185b
Alexander Gorban <gorban.alexander.y@gmail.com>
2019-07-23 19:04:29 +0300

    P88 Merge Sorted Array

---
+++ LeetCode/Solution/P88_MergeSortedArray/Solution.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Solution.P88_MergeSortedArray
+{
+    /*
+     * 88. Merge Sorted Array
+     * https://leetcode.com/problems/merge-sorted-array/
+     * Array, Two Pointers
+     * 
+     * Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as one sorted array.
+     * The number of elements initialized in nums1 and nums2 are m and n respectively.
+     * You may assume that nums1 has enough space (size that is greater or equal to m + n) to hold additional elements from nums2.
+     *
+     * Example:
+     * Input:
+     * nums1 = [1,2,3,0,0,0], m = 3
+     * nums2 = [2,5,6],       n = 3
+     * Output: [1,2,2,3,5,6]
+     */
+    class Solution
+    {
+        public void Merge(int[] nums1, int m, int[] nums2, int n)
+        {
+            if (nums2.Length == 0)
+            {
+                return;
+            }
+
+            int p = m - 1;
+            int q = n - 1;
+            int i = m + n - 1;
+
+            while (p >= 0 && q >= 0)
+            {
+                if (nums1[p] > nums2[q])
+                {
+                    nums1[i] = nums1[p];
+                    p--;
+                }
+                else
+                {
+                    nums1[i] = nums2[q];
+                    q--;
+                }
+
+                i--;
+            }
+
+            while (q >= 0)
+            {
+                nums1[i] = nums2[q];
+                q--;
+                i--;
+            }
+        }
+    }
+}
---
+++ LeetCode/Solution/P88_MergeSortedArray/SolutionTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace LeetCode.Solution.P88_MergeSortedArray
+{
+    public class SolutionTest
+    {
+        [Fact]
+        public void RemoveDuplicates()
+        {
+            int[] nums1 = new int[] { 1, 2, 3, 0, 0, 0 };
+            int m = 3;
+
+            int[] nums2 = new int[] { 2, 5, 6 };
+            int n = 3;
+
+            int[] target = new int[] { 1, 2, 2, 3, 5, 6 };
+
+            new Solution().Merge(nums1, m, nums2, n);
+
+            for (int i = 0; i < nums1.Length; i++)
+            {
+                Assert.Equal(target[i], nums1[i]);
+            }
+        }
+    }
+}
