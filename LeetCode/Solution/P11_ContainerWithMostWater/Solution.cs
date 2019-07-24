23080ca9caadfdceb1c2b4c2e923b37989ef586f
Alexander Gorban <gorban.alexander.y@gmail.com>
2019-07-24 22:50:16 +0300

    P11 Container With Most Water

---
+++ LeetCode/Solution/P11_ContainerWithMostWater/Solution.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Solution.P11_ContainerWithMostWater
+{
+    /*
+     * 11. Container With Most Water
+     * https://leetcode.com/problems/container-with-most-water/
+     * Array, Two Pointers
+     *
+     * Given n non-negative integers a1, a2, ..., an , where each represents a point at coordinate (i, ai). n vertical lines are drawn
+     * such that the two endpoints of line i is at (i, ai) and (i, 0). Find two lines, which together with x-axis forms a container,
+     * such that the container contains the most water.
+     *
+     * Note: You may not slant the container and n is at least 2.
+     *
+     * Example:
+     * Input: [1,8,6,2,5,4,8,3,7]
+     * Output: 49
+     */
+    class Solution
+    {
+        public int MaxArea(int[] height)
+        {
+            if (height == null || height.Length < 2)
+            {
+                return 0;
+            }
+
+            int left = 0;
+            int right = height.Length - 1;
+            int max = 0;
+
+            while (left < right)
+            {
+                int squareArea = (right - left) * Math.Min(height[left], height[right]);
+
+                if (squareArea > max)
+                {
+                    max = squareArea;
+                }
+
+                if (height[left] <= height[right])
+                {
+                    left++;
+                }
+                else
+                {
+                    right--;
+                }
+            }
+
+            return max;
+        }
+    }
+}
---
+++ LeetCode/Solution/P11_ContainerWithMostWater/SolutionTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace LeetCode.Solution.P11_ContainerWithMostWater
+{
+    public class SolutionTest
+    {
+        [Fact]
+        public void MaxArea()
+        {
+            int[] arrInts = new int[] { 1, 8, 6, 2, 5, 4, 8, 3, 7 };
+            int expectedSquare = 49;
+
+            Assert.Equal(expectedSquare, new Solution().MaxArea(arrInts));
+        }
+    }
+}
