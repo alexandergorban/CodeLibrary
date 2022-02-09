66ff001cc3337137c02b6749152b29b162b68fee
Alexander <gorban.alexander.y@gmail.com>
2022-02-09 12:52:29 +0200

    14. Longest Common Prefix

---
+++ LeetCode/Solution/P14_LongestCommonPrefix/Solution.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Solution.P14_LongestCommonPrefix
+{
+    /* 14. Longest Common Prefix
+     https://leetcode.com/problems/longest-common-prefix/
+     * string
+     *
+     * Write a function to find the longest common prefix string amongst an array of strings.
+     * If there is no common prefix, return an empty string "".
+
+     * Example 1:
+     * Input: strs = ["flower","flow","flight"]
+     * Output: "fl"
+
+     * Example 2:
+     * Input: strs = ["dog","racecar","car"]
+     * Output: ""
+     * Explanation: There is no common prefix among the input strings.
+ 
+
+     * Constraints:
+     * 1 <= strs.length <= 200
+     * 0 <= strs[i].length <= 200
+     * strs[i] consists of only lower-case English letters.
+     */
+    class Solution
+    {
+        public string LongestCommonPrefix(string[] strs)
+        {
+            if (strs.Length == 0)
+            {
+                return "";
+            }
+
+            string prefix = strs[0];
+
+            for (int i = 1; i < strs.Length; i++)
+            {
+                prefix = GetCommonPrefix(prefix, strs[i]);
+
+                if (prefix.Length == 0)
+                {
+                    return "";
+                }
+            }
+
+            return prefix;
+        }
+
+        private string GetCommonPrefix(string first, string second)
+        {
+            int length = Math.Min(first.Length, second.Length);
+
+            if (length == 0)
+            {
+                return "";
+            }
+
+            int i = 0;
+
+            while (i < length && first[i] == second[i])
+            {
+                i++;
+            }
+
+            return first.Substring(0, i);
+        }
+    }
+}
---
+++ LeetCode/Solution/P14_LongestCommonPrefix/SolutionTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace LeetCode.Solution.P14_LongestCommonPrefix
+{
+    public class SolutionTest
+    {
+        [Fact]
+        public void LongestCommonPrefix()
+        {
+            string[] strs = new string[] { "flower", "flow", "flight" };
+            string target = "fl";
+            Assert.Equal(target, new Solution().LongestCommonPrefix(strs));
+
+            string[] strs2 = new string[] { "dog", "racecar", "car" };
+            string target2 = "";
+            Assert.Equal(target2, new Solution().LongestCommonPrefix(strs2));
+        }
+    }
+}
